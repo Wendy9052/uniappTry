3dae0dad8e5d36991484ee5c80f2ad442fafa70f
wendy <wendy@renzhoulongs-iMac.local>
2021-12-14 14:15:03 +0800

    add_index

--- chat_proj/src/main.js
+++ chat_proj/src/main.js
@@ -3,6 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import JwChat from 'jwchat';
+Vue.use(ElementUI);
+/* 在 0.2.041 之前的版本需要引入 css */
+// import 'jwchat/lib/JwChat.css';
+Vue.use(JwChat)
+
 
 Vue.config.productionTip = false
 
---
+++ chat_proj/src/view/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    主页
+    <JwChat-index
+        :taleList="list"
+        @enter="bindEnter"
+        v-model="inputMsg"
+        :showRightBox='true'
+        scrollType="noscroll"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+      inputMsg: "",
+    }
+  },
+  methods: {
+    bindEnter() {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
